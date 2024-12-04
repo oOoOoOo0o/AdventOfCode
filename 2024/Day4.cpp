4ed7c91fdf9f74a7fb6fe13f37508a371ed512de
Bayan <b.nezamabad@gmail.com>
2024-12-04 10:02:36 +0000

    Day 4 improved part 1

--- 2024/Day4.cpp
+++ 2024/Day4.cpp
@@ -17,77 +17,48 @@ int main() {
     rows.push_back(line);
   }
 
-  // Part 1 (awful)
+  // Part 1
 
   int count = 0;
-
-  // Search horizontally
-  for (string row: rows) {
-    for (int i = 0; i < (row.size() - 3); i++) {
-      if (row[i] == 'X' &&
-          row[i + 1] == 'M' &&
-          row[i + 2] == 'A' &&
-          row[i + 3] == 'S') {
-        count++;
-      } else if (row[i] == 'S' &&
-                 row[i + 1] == 'A' &&
-                 row[i + 2] == 'M' &&
-                 row[i + 3] == 'X') {
-        count++;
-      }
-    }
-  }
-
-  // Search vertically
   int length = rows[0].length();
+  string s;
 
-  for (int i = 0; i < length; i++) {
-    for (int j = 0; j < (length - 3); j++) {
-      if (rows[j][i] == 'X' &&
-          rows[j + 1][i] == 'M' &&
-          rows[j + 2][i] == 'A' &&
-          rows[j + 3][i] == 'S') {
-        count++;
-      } else if (rows[j][i] == 'S' &&
-                 rows[j + 1][i] == 'A' &&
-                 rows[j + 2][i] == 'M' &&
-                 rows[j + 3][i] == 'X') {
-        count++;
+  for (int i = 0; i < (length); i++) {
+    for (int j = 0; j < (length); j++) {
+      // Horizontal search
+      if (j < length - 3) {
+        s = {rows[i][j], rows[i][j+1], rows[i][j+2], rows[i][j+3]};
+        if (s == "XMAS" || s == "SAMX") {
+          count++;
+        }
       }
-    }
-  }
 
-  // Search diagonally
-  for (int i = 0; i < (length - 3); i++) {
-    for (int j = 0; j < (length - 3); j++) {
-      if (rows[j][i] == 'X' &&
-          rows[j + 1][i + 1] == 'M' &&
-          rows[j + 2][i + 2] == 'A' &&
-          rows[j + 3][i + 3] == 'S') {
-        count++;
-      } else if (rows[j][i] == 'S' &&
-                 rows[j + 1][i + 1] == 'A' &&
-                 rows[j + 2][i + 2] == 'M' &&
-                 rows[j + 3][i + 3] == 'X') {
-        count++;
+      // Vertical search
+      if (i < length - 3) {
+        s = {rows[i][j], rows[i+1][j], rows[i+2][j], rows[i+3][j]};
+        if (s == "XMAS" || s == "SAMX") {
+          count++;
+        }
       }
-      if (rows[j + 3][i] == 'X' &&
-          rows[j + 2][i + 1] == 'M' &&
-          rows[j + 1][i + 2] == 'A' &&
-          rows[j][i + 3] == 'S') {
-        count++;
-      } else if (rows[j + 3][i] == 'S' &&
-                 rows[j + 2][i + 1] == 'A' &&
-                 rows[j + 1][i + 2] == 'M' &&
-                 rows[j][i + 3] == 'X') {
-        count++;
+
+      // Diagonal search
+      if (i < length - 3 && j < length - 3) {
+        s = {rows[i][j], rows[i+1][j+1], rows[i+2][j+2], rows[i+3][j+3]};
+        if (s == "XMAS" || s == "SAMX") {
+          count++;
+        }
+
+        s = {rows[i+3][j], rows[i+2][j+1], rows[i+1][j+2], rows[i][j+3]};
+        if (s == "XMAS" || s == "SAMX") {
+          count++;
+        }
       }
     }
   }
 
   cout << count << endl;
 
-  // Part 2 (nice!)
+  // Part 2
 
   count = 0;
 
