224070fdba1c807e2ce206158d5ef7eec1a70f3f
Bayan <b.nezamabad@gmail.com>
2024-12-11 11:04:05 +0000

    Day 11 unordered maps are really fast

--- 2024/Day11.cpp
+++ 2024/Day11.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <chrono>
 #include <cmath>
 
 using namespace std;
 
-void changeStones(vector<unsigned long long> &stones);
+void changeStones(unordered_map<unsigned long long, unsigned long long> &);
 int countDigits(unsigned long long n);
 
 int main() {
@@ -17,42 +18,59 @@ int main() {
   ifstream Input("2024/Day11Input.txt");
 
   string line;
-  vector<unsigned long long> stones;
+  unordered_map<unsigned long long, unsigned long long> stones;
   getline(Input, line);
 
   istringstream iss(line);
   unsigned long long x;
   while (iss >> x) {
-    stones.push_back(x);
+    stones[x] = 1;
   }
 
-  unsigned long long blinks = 25; // use 75 blinks for part 2
+  // unsigned long long blinks = 25; // part 1
+  unsigned long long blinks = 75; // part 2
 
   for (int i = 0; i < blinks; i++) {
     changeStones(stones);
   }
 
-  cout << stones.size() << endl;
+  unsigned long long total = 0;
+  for (pair kv : stones) {
+    total += kv.second;
+  }
+
+  cout << total << endl;
 
   auto end = chrono::high_resolution_clock::now();
   auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
   cout << "Execution time: " << duration.count() << "ms" << endl;
 }
 
-void changeStones(vector<unsigned long long> &stones) {
-  for (int i = 0; i < stones.size(); i++) {
-    unsigned long long num = stones[i];
+void changeStones(unordered_map<unsigned long long, unsigned long long> &stones) {
+  vector<pair<unsigned long long, unsigned long long>> stonesKvs;
+  stonesKvs.reserve(stones.size());
+  for (pair kv : stones) {
+    stonesKvs.emplace_back(kv);
+  }
+
+  for (int i = 0; i < stonesKvs.size(); i++) {
+    unsigned long long num = stonesKvs[i].first;
+    unsigned long long count = stonesKvs[i].second;
     if (num == 0) {
-      stones[i] = 1;
+      stones[num] -= count;
+      stones[1] += count;
     } else if (countDigits(num) % 2 == 0) {
       unsigned long long divisor = pow(10, countDigits(num) / 2);
       unsigned long long num1 = num / divisor;
       unsigned long long num2 = num % divisor;
-      stones[i] = num1;
-      stones.insert(stones.begin() + i + 1, num2);
-      i++;
+
+      stones[num] -= count;
+      stones[num1] += count;
+      stones[num2] += count;
     } else {
-      stones[i] = num * 2024;
+      stones[num] -= count;
+      num *= 2024;
+      stones[num] += count;
     }
   }
 }
